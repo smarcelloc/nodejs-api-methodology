652302b407fdd3a58635a3590a5510dd9f543181
Marcello Silvério <smarcelloc@gmail.com>
2021-12-09 16:39:09 -0300

    test: response validation of api external

--- src/clients/test/stormGlass.test.ts
+++ src/clients/test/stormGlass.test.ts
@@ -2,6 +2,7 @@ import StormGlass from '@src/clients/StormGlass';
 import axios from 'axios';
 import stormGlassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import stormglassIncompletePoint from '@test/fixtures/stormglass_incomplete_weather_3_hours.json';
 
 jest.mock('axios');
 
@@ -17,5 +18,19 @@ describe('StormGlass client', () => {
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.featchPoints(latitude, longitude);
     expect(response).toEqual(stormglassNormalizedResponseFixture);
+  });
+
+  it('should exclude incomplete data points', async () => {
+    const latitude = -33.792726;
+    const longitude = 151.289824;
+
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormglassIncompletePoint });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.featchPoints(latitude, longitude);
+
+    expect(response).toEqual([]);
   });
 });
