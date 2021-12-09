b4364b516d31a96ea299cb931b21da742ec796e3
Marcello Silvério <smarcelloc@gmail.com>
2021-12-09 16:58:41 -0300

    test: error handling for response and client request

--- src/clients/test/stormGlass.test.ts
+++ src/clients/test/stormGlass.test.ts
@@ -16,7 +16,7 @@ describe('StormGlass client', () => {
     mockAxios.get.mockResolvedValue({ data: stormGlassWeatherPointFixture });
 
     const stormGlass = new StormGlass(axios);
-    const response = await stormGlass.featchPoints(latituteFake, longitudeFake);
+    const response = await stormGlass.fetchPoints(latituteFake, longitudeFake);
     expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
 
@@ -24,8 +24,37 @@ describe('StormGlass client', () => {
     mockAxios.get.mockResolvedValue({ data: stormglassIncompletePoint });
 
     const stormGlass = new StormGlass(axios);
-    const response = await stormGlass.featchPoints(latituteFake, longitudeFake);
+    const response = await stormGlass.fetchPoints(latituteFake, longitudeFake);
 
     expect(response).toEqual([]);
+  });
+
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
+    mockAxios.get.mockRejectedValue({ message: 'Network Error' });
+
+    const stormGlass = new StormGlass(mockAxios);
+
+    await expect(
+      stormGlass.fetchPoints(latituteFake, longitudeFake)
+    ).rejects.toThrow(
+      'Unexpected error when trying to communicate to StormGlass: Network Error'
+    );
+  });
+
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    mockAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockAxios);
+
+    await expect(
+      stormGlass.fetchPoints(latituteFake, longitudeFake)
+    ).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
   });
 });
