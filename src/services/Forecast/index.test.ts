ece1de3a27acc32d8a4ea9d314b51166494619b2
Marcello Silvério <smarcelloc@gmail.com>
2021-12-10 19:24:59 -0300

    refactor: forecast service in test file

--- src/services/Forecast/index.test.ts
+++ src/services/Forecast/index.test.ts
@@ -1,5 +1,6 @@
 import StormGlass from '@src/clients/StormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import forecastListBeaches from '@test/fixtures/forecast_list_beaches.json';
 import ForecastService from '.';
 import { ForecastProcessingServerError } from './error';
 import { Beach, BeachPosition } from './interface';
@@ -7,88 +8,25 @@ import { Beach, BeachPosition } from './interface';
 jest.mock('@src/clients/StormGlass');
 
 describe('Forecast Service', () => {
+  const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
+
+  const beaches: Beach[] = [
+    {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.EAST,
+      user: 'some-id',
+    },
+  ];
+
   it('should return the forecast for a list of beaches', async () => {
-    StormGlass.prototype.fetchPoints = jest
-      .fn()
-      .mockResolvedValue(stormGlassNormalizedResponseFixture);
+    mockedStormGlassService.fetchPoints.mockResolvedValue(stormGlassNormalizedResponseFixture);
 
-    const beaches: Beach[] = [
-      {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: BeachPosition.EAST,
-        user: 'some-id',
-      },
-    ];
-
-    const expectedResponse = [
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T02:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 182.56,
-            swellHeight: 0.28,
-            swellPeriod: 3.44,
-            time: '2020-04-26T02:00:00+00:00',
-            waveDirection: 232.86,
-            waveHeight: 0.46,
-            windDirection: 321.5,
-            windSpeed: 100,
-          },
-        ],
-      },
-    ];
-
-    const forecast = new ForecastService();
+    const forecast = new ForecastService(mockedStormGlassService);
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
 
-    expect(beachesWithRating).toEqual(expectedResponse);
+    expect(beachesWithRating).toEqual(forecastListBeaches);
   });
 
   it('should return an empty list when the beaches array is empty', async () => {
@@ -99,19 +37,9 @@ describe('Forecast Service', () => {
   });
 
   it('should throw internal processing error when something goes wrong during the rating process', async () => {
-    const beaches: Beach[] = [
-      {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: BeachPosition.EAST,
-        user: 'some-id',
-      },
-    ];
+    mockedStormGlassService.fetchPoints.mockRejectedValue('Error fetching data');
 
-    StormGlass.prototype.fetchPoints = jest.fn().mockRejectedValue('Error fetching data');
-
-    const forecast = new ForecastService(new StormGlass());
+    const forecast = new ForecastService(mockedStormGlassService);
     await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrowError(
       ForecastProcessingServerError
     );
