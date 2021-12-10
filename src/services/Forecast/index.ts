504c002fffd10f90d0d1617645eda23a9886e4ee
Marcello Silvério <smarcelloc@gmail.com>
2021-12-10 16:57:37 -0300

    refactor: TDD (Refactor): teste do forecast.

--- src/services/Forecast/index.ts
+++ src/services/Forecast/index.ts
@@ -1,4 +1,5 @@
 import StormGlass from '@src/clients/StormGlass';
+import { ForecastPoint } from '@src/clients/StormGlass/interfaces';
 import { Beach, BeachForecast } from './interface';
 
 class ForecastService {
@@ -9,20 +10,21 @@ class ForecastService {
 
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-
-      const enrichedBeachData = points.map((element) => ({
-        lat: beach.lat,
-        lng: beach.lng,
-        name: beach.name,
-        position: beach.position,
-        rating: 1, // need to be implemented
-        ...element,
-      }));
-
-      pointsWithCorrectSources.push(...enrichedBeachData);
+      pointsWithCorrectSources.push(...this.enrichBeachData(points, beach));
     }
 
     return pointsWithCorrectSources;
+  }
+
+  private enrichBeachData(points: ForecastPoint[], beach: Beach): BeachForecast[] {
+    return points.map((element) => ({
+      lat: beach.lat,
+      lng: beach.lng,
+      name: beach.name,
+      position: beach.position,
+      rating: 1, // need to be implemented
+      ...element,
+    }));
   }
 }
 
