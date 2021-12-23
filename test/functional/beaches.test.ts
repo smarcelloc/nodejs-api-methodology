ff3ed922b1693be0c75315904b6774ea8c238090
Marcello Silvério <smarcelloc@gmail.com>
2021-12-23 13:02:49 -0300

    test: create test for error in mongoose

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -16,4 +16,23 @@ describe('Beaches functional tests', () => {
 
     expect(response.body).toEqual(expect.objectContaining(newBeach));
   });
+
+  it('should return 422 when there is a validation error', async () => {
+    const newBeach = {
+      lat: 'invalid_string',
+      lng: 151.289824,
+      name: 'Manly',
+      position: 'E',
+    };
+
+    const response = await global.testRequest.post('/beaches').send(newBeach);
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      error:
+        'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+    });
+  });
+
+  it.skip('should return 500 when there is any error other than validation error', async () => {});
 });
