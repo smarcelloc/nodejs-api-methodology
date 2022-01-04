edd880b5df6a9270b329e13c7c4a9a3d01f78682
Marcello Silvério <smarcelloc@gmail.com>
2022-01-04 16:56:17 -0300

    test: create test functional for forecast request with Storm Glass

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,6 +1,22 @@
+import { Beach, BeachPosition, BeachProps } from '@src/model/Beach';
+
 import forecastListBeaches from '@test/fixtures/forecast_list_beaches.json';
 
 describe('Beach forecast functional tests', () => {
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+
+    const defaultBeach: BeachProps = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.EAST,
+    };
+
+    const beach = new Beach(defaultBeach);
+    await beach.save();
+  });
+
   it('should return a forecast with just a few times', async () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
