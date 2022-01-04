99257e7028a86e92cd8f960cb943d4bbe720e049
Marcello Silvério <smarcelloc@gmail.com>
2022-01-04 17:21:26 -0300

    test: create test Forecast for return exception 500

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -21,28 +21,41 @@ describe('Beach forecast functional tests', () => {
   });
 
   it('should return a forecast with just a few times', async () => {
-    nockSimulateStormGlass();
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlassWeather3HoursFixture);
 
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual(forecastListBeaches);
   });
-});
 
-const nockSimulateStormGlass = () => {
-  nock('https://api.stormglass.io:443', {
-    encodedQueryParams: true,
-    reqheaders: {
-      Authorization: (): boolean => true,
-    },
-  })
-    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-    .get('/v2/weather/point')
-    .query({
-      lat: '-33.792726',
-      lng: '151.289824',
-      params: /(.*)/,
-      source: 'noaa',
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
     })
-    .reply(200, stormGlassWeather3HoursFixture);
-};
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v1/weather/point')
+      .query({ lat: '-33.792726', lng: '151.289824' })
+      .replyWithError('Something went wrong');
+
+    const { status } = await global.testRequest.get(`/forecast`);
+
+    expect(status).toBe(500);
+  });
+});
