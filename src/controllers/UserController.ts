9b2e30ae3faadce9856c0f7ad4e35e5b733276f4
Marcello Silvério <smarcelloc@gmail.com>
2022-01-04 21:38:06 -0300

    feat: error handling in User Controller for rest post

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import UserModel from '@src/models/User';
-
 @Controller('users')
 class UserController {
   @Post()
   public async create(req: Request, res: Response) {
-    const user = new UserModel(req.body);
-    const result = await user.save();
-    res.status(201).send(result);
+    try {
+      const user = new UserModel(req.body);
+      const result = await user.save();
+      res.status(201).send(result);
+    } catch (error: any) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
 
