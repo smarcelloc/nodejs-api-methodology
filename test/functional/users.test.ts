06850228691829bd8576c48df4312f45eb2c23ba
Marcello Silvério <smarcelloc@gmail.com>
2022-01-06 09:56:38 -0300

    test(functional): user access ANAUTHORIZED by JWT

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -74,4 +74,26 @@ describe('When authenticating a user', () => {
       expect.objectContaining({ token: expect.any(String) })
     );
   });
+
+  it('Should return UNAUTHORIZED if the user with the given email is not found', async () => {
+    const response = await global.testRequest
+      .post('/users/authenticate')
+      .send({ email: 'some-email@mail.com', password: '1234' });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('Should return ANAUTHORIZED if the user is found but the password does not match', async () => {
+    const newUser = {
+      name: 'John Doe',
+      email: 'john@mail.com',
+      password: '1234',
+    };
+    await new UserModel(newUser).save();
+    const response = await global.testRequest
+      .post('/users/authenticate')
+      .send({ email: newUser.email, password: 'different password' });
+
+    expect(response.status).toBe(401);
+  });
 });
