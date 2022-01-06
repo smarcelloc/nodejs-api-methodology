35f32813d2004ac36490bdd66a200d6d0e9333a1
Marcello Silvério <smarcelloc@gmail.com>
2022-01-06 08:43:07 -0300

    feat: add try-catch for auth middleware UNAUTHORIZED

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -7,11 +7,19 @@ const authMiddleware = (
   res: Partial<Response>,
   next: NextFunction
 ): void => {
-  const token = req.headers?.['x-access-token'];
+  try {
+    const token = req.headers?.['x-access-token'];
 
-  const decoded = AuthService.decodeToken(token as string);
-  req.decoded = decoded;
-  next();
+    const decoded = AuthService.decodeToken(token as string);
+    req.decoded = decoded;
+    next();
+  } catch (error: any) {
+    if (error instanceof Error) {
+      res.status?.(401).send({ code: 401, error: error.message });
+    } else {
+      res.status?.(401).send({ code: 401, error: 'Unknown auth error' });
+    }
+  }
 };
 
 export default authMiddleware;
