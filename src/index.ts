4a035938e0e2527819ea63dfbcd2474eb52ea44c
Marcello Silvério <smarcelloc@gmail.com>
2022-02-12 14:35:20 -0300

    feat: good practices using the  Graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
 import SetupServer from '@src/server';
+import logger from '@src/util/logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on('unhandledRejection', (reason: unknown, promise: Promise<unknown>) => {
+  logger.error(`App exiting due to an unhandled promise: ${promise} and reason: ${reason}`);
+  throw reason;
+});
+
+process.on('uncaughtException', (error: Error) => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
 
 (async (): Promise<void> => {
-  const server = new SetupServer();
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer();
+    await server.init();
+    server.start();
+
+    const exitSignals: globalThis.NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close();
+          logger.info(`App exited with success`);
+          process.exit(ExitStatus.Success);
+        } catch (error: any) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error: any) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
